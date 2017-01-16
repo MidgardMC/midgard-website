b0535edc261f17e118ad7adb73d39cb51b5d6523
Caleb Flatten <spinnibbler@users.noreply.github.com>
2017-01-16 12:56:08 -0600

    Content Update

---
+++ 360/ngthome/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2017-01-16_121033_stitch",
+      "name": "2017-01-16_12.10.33_stitch",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1408,
+      "initialViewParameters": {
+        "yaw": -2.9066081362337464,
+        "pitch": 0.06112778776875061,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
