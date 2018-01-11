8825ea06dd016d2149bea997d82f03309d61131d
joshkingwv <joshkingwv@gmail.com>
2018-01-11 14:14:35 -0500

    new website - 1/11/2017

---
+++ header.php
@@ -0,0 +1,52 @@
+<head>
+        <title>Midgard Minecraft Server</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <link href="assets/main.css" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <script defer src="https://use.fontawesome.com/releases/v5.0.2/js/all.js"></script>
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700|Roboto+Slab:400,700" rel="stylesheet"> 
+    </head>
+    <body>
+        <div class="wrapper">
+
+            <div class="container-fluid header">
+            <div class="row">
+
+                <div class="col-md-4">
+                <a href="index.php"><img src="img/logo.png" class="img-responsive logo" /></a>
+                </div>
+
+                <div class="col-md-8 menu">
+
+                    <nav class="navbar navbar-default">
+                        <div class="container-fluid nav">
+                            <!-- Brand and toggle get grouped for better mobile display -->
+                            <div class="navbar-header">
+                            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                                <span class="sr-only">Toggle navigation</span>
+                                <i class="fa fa-bars fa-2x"></i>
+                            </button>
+                            </div>
+
+                            <!-- Collect the nav links, forms, and other content for toggling -->
+                            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li><a href="index.php">Home <span class="sr-only">(current)</span></a></li>
+                                    <li><a href="about.php">About</a></li>
+                                    <!--<li><a href="#">Servers</a></li>-->
+                                    <li><a href="http://imgur.com/a/0GWt1" target="_blank">Gallery</a></li>
+                                    <li><a href="map.php">Map</a></li>
+                                    <li><a href="rules.php">Rules</a></li>
+                                    <li class="menu-apply"><a href="https://goo.gl/forms/CBCx3wDhFkyHfpPf1" target="_blank">Apply</a></li>
+                                </ul>
+                            </div><!-- /.navbar-collapse -->
+                        </div><!-- /.container-fluid -->
+
+                    </div>
+
+                </div>
+                </div>
+
+
