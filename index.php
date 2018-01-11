084c518bca37769dac6a61da99d41f8e3e4410ea
joshkingwv <joshkingwv@gmail.com>
2018-01-11 14:41:07 -0500

    Adding new website

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php include('header.php'); ?>
+
+<div class="jumbotron text-center">
+  <h1>Minecraft for the Masters</h1>
+  <p>Midgard is a Minecraft community for experienced players who want an enhanced vanilla experience.</p>
+  <p><a class="btn btn-success btn-lg" href="https://goo.gl/forms/CBCx3wDhFkyHfpPf1" target="_blank" role="button">Apply Now</a></p>
+</div>
+
+<div class="container-fluid about front-features">
+  <div class="row">
+    <div class="col-md-4 col-sm-12 col-xs-12">
+      <div class="fa-10x">
+        <span class="fa-layers fa-fw">
+          <i class="fas fa-circle" style="color:purple"></i>
+          <i class="fa-inverse far fa-money-bill-alt" data-fa-transform="shrink-7"></i>
+        </span>
+      </div>
+        <h2>Get Paid To Play</h3>
+        <p>Minecraft can be a job in an of itself, no need to "work", you can buy, sell, trade and get daily payouts of Coins (our in-game currency) to keep you going.</p>
+    </div>
+    <div class="col-md-4 col-sm-12 col-xs-12">
+        <div class="fa-10x">
+          <span class="fa-layers fa-fw">
+            <i class="fas fa-circle" style="color:purple"></i>
+            <i class="fa-inverse far fa-map" data-fa-transform="shrink-7"></i>
+          </span>
+        </div>
+        <h2>Worry-Free Experience</h3>
+        <p>Constant looming threat of your server's map being reset or going offline permanently? We've been here for over 6 years. We're not going anywhere.</p>
+    </div>
+    <div class="col-md-4 col-sm-12 col-xs-12">
+        <div class="fa-10x">
+          <span class="fa-layers fa-fw">
+            <i class="fas fa-circle" style="color:purple"></i>
+            <i class="fa-inverse fas fa-cog" data-fa-transform="shrink-7"></i>
+          </span>
+        </div>
+        <h2>Resource World</h3>
+        <p>Need lots of stone or dirt for your next big project? No problem. Our resource World, resource Nether, and resource End resets every 90-120 days.</p>
+    </div>
+  </div>
+</div>
+
+<div class="container-fluid about a-2">
+  <div class="row">
+    <div class="col-md-12 col-sm-12 col-xs-12">
+      <h2>Looking for a more mature experience?</h2>
+      <p>Join other experienced and passionate players in our community. Apply for our whitelist today.</p>
+      <p><a class="btn btn-success btn-md" href="https://goo.gl/forms/CBCx3wDhFkyHfpPf1" target="_blank" role="button">Apply Now</a></p>
+    </div>
+  </div>
+</div>
+
+<?php include('footer.php'); ?>
---
+++ map.php
@@ -0,0 +1,7 @@
+<?php include('header.php'); ?>
+
+    <iframe src="http://mc.midgardmc.com:8123/" style="height:100%; width:100%;" frameborder="0">
+        <p>Your browser does not support iframes.</p>
+    </iframe>
+         
+<?php include('footer.php'); ?>
